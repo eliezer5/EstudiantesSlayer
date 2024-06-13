789b6741946ae7591654b8ef4ee6636ceb964571
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-06-12 21:45:51 -0400

    creando proyecto

---
+++ Models/SemestreH.cs
@@ -0,0 +1,12 @@
+﻿namespace facEstudiante.Models
+{
+    public class SemestreH
+    {
+
+        public string? Nombre { get; set; }
+        public string? Codigo { get; set; }
+        public int? Creditos { get; set; }
+        public string? Profesor { get; set; }
+
+    }
+}
