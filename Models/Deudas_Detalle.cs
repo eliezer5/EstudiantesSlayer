badd22908f5b4f445e04eceb337f2d5fc35109c9
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-06-13 20:20:44 -0400

    the rest

---
+++ Models/Deudas_Detalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace facEstudiante.Models
+{
+    public class Deudas_Detalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        [ForeignKey("DeudaId")]
+        public int DeudaId { get; set; }
+
+        DateTime Fecha { get; set; }
+
+        public string Periodo { get; set; }= string.Empty;
+
+        public int Monto { get; set; }
+
+
+    }
+}
